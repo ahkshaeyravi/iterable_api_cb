52fb05eac9d192df8eaf51e6d90a46b5dfcb4b26
Ahkshaey Ravi <ahkshaey@gmail.com>
2024-08-14 23:04:25 +0530

    updated all routes info

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "trigger_event_a" => "welcome#trigger_event_a", as: :trigger_event_a
-  get "trigger_event_b" => "welcome#trigger_event_b", as: :trigger_event_b
+  get "trigger_event_a" => "events#trigger_event_a", as: :trigger_event_a
+  get "trigger_event_b" => "events#trigger_event_b", as: :trigger_event_b
 
   # Defines the root path route ("/")
-  root "welcome#index"
+  root "events#index"
 end
