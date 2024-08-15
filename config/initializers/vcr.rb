0bdf120724cd05cefb8d973dc9f3b97843c64111
Ahkshaey Ravi <ahkshaey@gmail.com>
2024-08-15 15:17:48 +0530

    small chnages in vcr init file

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -5,7 +5,7 @@ require 'webmock'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock
-  config.configure_rspec_metadata!
+  config.configure_rspec_metadata! if defined?(RSpec)
   config.filter_sensitive_data('<ITERABLE_API_KEY>') { ENV['ITERABLE_API_KEY'] || 'fake_api_key' }
   config.allow_http_connections_when_no_cassette = true # Allow real requests if no cassette is found
 
